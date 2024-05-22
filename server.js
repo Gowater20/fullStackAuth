c896c2a9237d791e7cac163d929e4cfea80e3782
Fabio Vallacqua <fabio.vallacqua@gmail.com>
2024-05-22 22:22:50 +0200

    feat: access token during login

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const secretKey = "your_secret_key";
 mongoose.connect("mongodb://localhost:27017/fullStack");
-
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
@@ -69,31 +70,36 @@ app.post("/register-user", async (req, res) => {
         res.status(500).json("internal server error");
     }
 });
-
 app.post("/login-user", async (req, res) => {
     const { email, password } = req.body;
 
     try {
         // Trova l'utente per email
         const user = await User.findOne({ email });
-
         if (!user) {
+            console.log("User not found for email:", email);
             res.json("email or password is incorrect");
             return;
         }
 
         // Confronta la password fornita con quella salvata
         const isMatch = await bcrypt.compare(password, user.password);
-
         if (isMatch) {
-            res.json(user);
+            // Genera un token JWT
+            const token = jwt.sign({ id: user._id, email: user.email }, secretKey, { expiresIn: '1d' });
+            console.log("token", token);
+
+            // Restituisce il token insieme ai dati dell'utente
+            res.json({ user, token });
         } else {
             res.json("email or password is incorrect");
         }
     } catch (err) {
+        console.error("Error during login process:", err);
         res.status(500).json("internal server error");
     }
 });
+
 
 app.listen(3000, (req, res) => {
 	console.log("listening on port 3000......");
